20140bbfa1de309e1123406b82c2079880367ea3
Charlie Taylor <peekoecode@gmail.com>
2024-05-10 21:37:37 -0700

    CRUD 🐒

---
+++ server/crud.py
@@ -0,0 +1,37 @@
+from typing import List
+import models, schema
+from sqlalchemy.orm import Session
+
+
+def get_user(db: Session, username: str) -> models.User | None:
+    return db.query(models.User).filter(models.User.username == username).first()
+
+
+def get_user_by_phone(db: Session, phone: str) -> models.User | None:
+    return db.query(models.User).filter(models.User.phone == phone).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100) -> List[models.User]:
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schema.User) -> models.User:
+    db_user = models.User(username=user.username, phone=user.phone, active=True)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_registration(db: Session, phone: str) -> models.Registration | None:
+    return (
+        db.query(models.Registration).filter(models.Registration.phone == phone).first()
+    )
+
+
+def create_registration(db: Session, reg: schema.Registrations) -> models.Registration:
+    db_reg = models.Registration(phone=reg.phone, state=0)
+    db.add(db_reg)
+    db.commit()
+    db.refresh(db_reg)
+    return db_reg
--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 from typing import Generator
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
 from config import settings
 
@@ -7,8 +7,9 @@ from sqlalchemy.orm import Session
 
 from database import SessionLocal, engine, get_db
 
-import models
-import schema
+import schema, models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -17,17 +18,6 @@ def read_root():
     db = get_db()
     user = None
     if isinstance(db, Session):
-        user = db.query(models.User).first()
+        user = db.query(schema.User).first()
 
     return user
-
-@app.get("/test")
-def test():
-    db = get_db()
-    us = models.User(username="test", phone="123-456-7890", is_active=True)
-    if isinstance(db, Session):
-        db.add(us)
-        db.commit()
-        db.refresh(us)
-
-    return us

e575111da6803a8f779da0e6bfb34e7a8f98dfec
AaronLieb <aarnliebrmn@gmail.com>
2024-08-21 13:32:48 -0700

    twilio webhook boilerplate

--- server/main.py
+++ server/main.py
@@ -1,5 +1,7 @@
 from typing import Generator
+
 from fastapi import Depends, FastAPI, UploadFile
+from twilio.twiml.messaging_response import MessagingResponse
 
 from config import settings
 import crud
@@ -7,12 +9,14 @@ import crud
 from sqlalchemy.orm import Session
 
 from database import SessionLocal, engine, get_db
+from sms import SmsClient
 
 import schema, models
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+twilio_client = SmsClient()
 
 @app.get("/")
 def read_root(db: Session = Depends(get_db)):
@@ -30,3 +34,8 @@ def upload(user_hash: str, file: UploadFile, db: Session = Depends(get_db)):
         return None
 
     return crud.create_pic(db, user.username, file)
+
+@app.post("/sms")
+def receive_message(from_: str = Form(...), body: str = Form(...)):
+    twilio_client.receive_message(from_, body)
+    twilio_client.send_message(from_, "Received message {}".format(body))

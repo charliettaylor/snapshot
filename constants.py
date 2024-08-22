4745469c6c464fec6e36120dac69372917b3d11a
AaronLieb <aarnliebrmn@gmail.com>
2024-08-22 12:30:18 -0700

    small change

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ SNAPSHOT_MULTI_LINE = "Snapshot 📸"
 HOW_TO_START = SNAPSHOT + "Text START to play."
 ENTER_USERNAME = (
     SNAPSHOT_MULTI_LINE
-    + "\nText STOP to unsubscribe.\n\n To finish registering, please enter your username:"
+    + "\nText STOP to unsubscribe.\n\nTo finish registering, please enter your username:"
 )
 ENTER_USERNAME_AGAIN = SNAPSHOT + "please enter your username:"
 CONFIRM_USERNAME = SNAPSHOT + 'You entered "{}", text YES to confirm or NO to change.'
